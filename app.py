a62731fe356c203be7ae93cefb925387a75ff677
Toni Räsänen <toni.rasanen@iki.fi>
2019-02-06 21:46:43 +0200

    add comics, refactor

--- app.py
+++ app.py
@@ -6,6 +6,38 @@ from io import BytesIO
 
 
 
+def scrape_daily_comic_strip(comimc_path):
+    DEBUG = False
+    SCHEMA = 'https://'
+    URL_HS = SCHEMA + 'www.hs.fi' 
+    URL_COMIC = URL_HS + comimc_path
+
+    res = requests.get(URL_COMIC)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    image_link_url = URL_HS + soup.select('.cartoon-content')[0].select('a')[0]['href']
+    
+    if (DEBUG):
+        print('image_link_url: ', image_link_url)
+
+    res = requests.get(image_link_url)
+    soup = BeautifulSoup(res.text, 'html.parser')
+
+    dirty_comic_url = soup.select('.scroller')[0].select('img')[0]['data-srcset']
+    comic_url = SCHEMA + dirty_comic_url.split()[0][2:] + '.webp'
+
+    if (DEBUG):
+        print('imgage url:', comic_url)
+
+    # TODO: check and maybe refactor
+    res = requests.get(comic_url)
+    image = Image.open(BytesIO(res.content))
+    imgByteArr = BytesIO()
+    image.save(imgByteArr, format='PNG')
+    imgByteArr = imgByteArr.getvalue()
+    return imgByteArr
+
+
+
 # Falcon follows the REST architectural style, meaning (among
 # other things) that you think in terms of resources and state
 # transitions, which map to HTTP verbs.
@@ -21,52 +53,38 @@ class QuoteResource(object):
 
 class ViiviWagner(object):
     def on_get(self, req, resp): 
-        resp.status = falcon.HTTP_200
-        resp.body = '\nDaily Viivi and Wagner\n\n'
-
-
-class FokIt(object):
-    def on_get(self, req, resp): 
-        resp.status = falcon.HTTP_200
-        resp.body = '\nDaily Fok_It\n\n'        
-
-
-class Fingerpori(object):
-    def on_get(self, req, resp): 
-        DEBUG = False
-        SCHEMA = 'https://'
-        URL_HS = SCHEMA + 'www.hs.fi'
-        URL_FINGERPORI = URL_HS + '/fingerpori/'
-
+        COMIC_PATH = '/viivijawagner/'
         try:
-            res = requests.get(URL_FINGERPORI)
-            soup = BeautifulSoup(res.text, 'html.parser')
-            image_link_url = URL_HS + soup.select('.cartoon-content')[0].select('a')[0]['href']
-            
-            if (DEBUG):
-                print('image_link_url: ', image_link_url)
-
-            res = requests.get(image_link_url)
-            soup = BeautifulSoup(res.text, 'html.parser')
-
-            dirty_comic_url = soup.select('.scroller')[0].select('img')[0]['data-srcset']
-            comic_url = SCHEMA + dirty_comic_url.split()[0][2:] + '.webp'
-
-            if (DEBUG):
-                print('imgage url:', comic_url)
-
-            # TODO: check and maybe refactor
-            res = requests.get(comic_url)
-            image = Image.open(BytesIO(res.content))
-            imgByteArr = BytesIO()
-            image.save(imgByteArr, format='PNG')
-            imgByteArr = imgByteArr.getvalue()
-            resp.body = imgByteArr
+            resp.body = scrape_daily_comic_strip(COMIC_PATH)
             resp.content_type = falcon.MEDIA_PNG
             resp.status = falcon.HTTP_200
         except:
             resp.status = falcon.HTTP_500
-            resp.body('ERROR ERROR')
+            resp.body('ERROR ERROR')   
+
+
+class FokIt(object):
+    def on_get(self, req, resp): 
+        COMIC_PATH = '/nyt/fokit/'
+        try:
+            resp.body = scrape_daily_comic_strip(COMIC_PATH)
+            resp.content_type = falcon.MEDIA_PNG
+            resp.status = falcon.HTTP_200
+        except:
+            resp.status = falcon.HTTP_500
+            resp.body('ERROR ERROR')                
+
+
+class Fingerpori(object):
+    def on_get(self, req, resp): 
+        COMIC_PATH = '/fingerpori/'
+        try:
+            resp.body = scrape_daily_comic_strip(COMIC_PATH)
+            resp.content_type = falcon.MEDIA_PNG
+            resp.status = falcon.HTTP_200
+        except:
+            resp.status = falcon.HTTP_500
+            resp.body('ERROR ERROR')  
 
 
 # falcon.API instances are callable WSGI apps
@@ -81,5 +99,5 @@ fokit = FokIt()
 # request handlers for paths
 app.add_route('/quotes', quotes)
 app.add_route('/finger', fingerpori)
-app.add_route('/viiviw', viiviw)
+app.add_route('/viivijawagner', viiviw)
 app.add_route('/fokit', fokit)
